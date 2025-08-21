dc5c76aa8867d10f4094a2623afea9ed75e4a813
ai-asiet <cseaiprojects@adishankara.ac.in>
2025-08-21 10:31:52 +0530

    Update loader.js

--- loader.js
+++ loader.js
@@ -4,8 +4,8 @@
         backendUrl: "https://your-backend-url.com",
         iconUrl: "https://ai-asiet.github.io/apiloader/icon%20cc%20st%20bnd.png",
         audioUrl: "music.mp3",
-        // iframeUrl: "https://jo-459671676510.us-central1.run.app",
-        iframeUrl: "https://apiloader.vercel.app/maintenance.html",
+        iframeUrl: "https://fc-yugs.onrender.com",
+        // iframeUrl: "https://apiloader.vercel.app/maintenance.html",
     };
 
     // Load confetti library if not already loaded
